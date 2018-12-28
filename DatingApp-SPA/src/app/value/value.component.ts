8b6a61066aa26e0a6d992d3eeb0b2381fa17b07b
Peeyal Khondokar <peeyalk@hotmail.com>
2018-12-28 07:45:12 +0600

    Dhaka to mym

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  values: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
