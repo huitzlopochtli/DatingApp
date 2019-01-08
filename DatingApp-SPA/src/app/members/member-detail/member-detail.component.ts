9d92ca5fc20bd9cc499e697bcdf708a04a5555e7
Peeyal Khondokar <peeyalk@hotmail.com>
2019-01-08 20:51:45 +0600

    implemented ngx-gallery

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { UserService } from 'src/app/_services/user.service';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,13 +12,15 @@ import { UserService } from 'src/app/_services/user.service';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(
     private userService: UserService,
     private alertify: AlertifyService,
     private route: ActivatedRoute,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => this.user = data['user']);
@@ -25,6 +28,30 @@ export class MemberDetailComponent implements OnInit {
       this.alertify.error('Problem retrieving user');
       this.router.navigate(['/members']);
     }
-  }
 
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        bog: this.user.photos[i].url,
+        url: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
+  }
 }
